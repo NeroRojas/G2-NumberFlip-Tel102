d23fc0bc1925d864a490778f66e4cbedf91fe1b9
JAVIER ROJAS LLANTEN <jrojasl@usm.cl>
2025-09-12 01:12:04 -0300

    Add files via upload

--- header.h
+++ header.h
@@ -9,9 +9,15 @@ typedef struct {
     int celda[N][N];
     int bombas_fila[N];
     int bombas_col[N];
+
+    int suma_fila[N];
+    int suma_col[N];
 } Board;
 
 int crear_tablero(Board *b);
 void mostrar_tablero(const Board *b, int mostrar_valores);
+
+void sumar_filas_columnas(Board *b);
+void mostrar_casillas(const Board *b, int vista[N][N]);
 
 #endif
